0b580e2fae113e5970b7fd4e338dbccbd198fcf3
Saphah Central Services (HUB) <bessingerbackup2024@gmail.com>
2025-09-24 08:45:24 +0200

    Create email-config.js

---
+++ email-config.js
@@ -0,0 +1,7 @@
+// email-config.js
+export const config = {
+  serviceName: "saphahemailservices",
+  retryLimit: 3,
+  queueInterval: 5000, // ms
+  defaultFrom: "saphahcentralservices@gmail.com"
+};
